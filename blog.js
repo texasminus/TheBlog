6aa904891565b9f1414e57bb0172dd4f365cf858
nws0078 <30375118+nws0078@users.noreply.github.com>
2022-05-24 23:51:18 -0400

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,12 @@
+createArticle.addEventListener('click', function(){
+    let list = document.getElementById("list");
+    let element = document.createElement("a");
+    let title = document.createTextNode(document.getElementById("title").value);
+    let content = document.createTextNode(document.getElementById("content").value);
+    let data = new Object();
+    element.appendChild(title);
+    list.appendChild(element);
+
+});
+
+
