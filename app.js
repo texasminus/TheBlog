d2eef03483adb05a613e7d199e1c2d37804e01d9
nws0078 <nws0078@gmail.com>
2022-06-01 12:38:57 -0400

    admin finished

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+const session = require('express-session');
+const MongoDBSession = require('connect-mongodb-session')(session);
+
+const mongoURI = 'mongodb+srv://nws0078:Park8785^^@apal.oi4gf.mongodb.net/?retryWrites=true&w=majority';
+
+
+
 // //Data Processing Middlewares
 app.use(express.urlencoded({extended: false}));
 
@@ -14,9 +21,33 @@ app.use(express.json());
 mongoose.connect('mongodb+srv://nws0078:Park8785^^@apal.oi4gf.mongodb.net/?retryWrites=true&w=majority');
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+db.once('open', () => console.log('Connected to Database\n'));
 
-//Static Folder 'public'
+
+// -----------------Session-----------------
+const store = new MongoDBSession({
+    uri: mongoURI,
+    collection: 'mySessions'
+});
+
+app.use(session({
+    secret: '1234',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  }));
+
+//Middleware to prevent accessing a page that needs auth.
+const isAuth = (req, res, next) => {
+    if(req.session.isAuth) {
+        next();
+    } else {
+        res.redirect('/');
+    };
+};
+
+
+//--------Static Folder 'public'--------
 app.use(express.static(path.join(__dirname,'public')));
 
 // -----------------GET-----------------
@@ -26,7 +57,7 @@ app.get('/', (req, res) => {
 });
 
 // Login 
-app.get('/loggedin', (req, res) => {
+app.get('/loggedin', isAuth,(req, res) => {
     res.sendFile(__dirname + '/views/loggedin.html');
 });
 
@@ -49,14 +80,23 @@ app.get('/blog', (req, res) => {
     res.sendFile(__dirname + '/views/blog.html');
 });
 
+// Logout
+app.post('/logout', (req, res) => {
+    req.session.destroy((err)=>{
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
 
-// Importing a router for Admin
-// All requests to API begin with /users
-const usersRouter = require('./routes/users');
+
+// ----------------Middlewares---------------- 
+// All requests to API begin with /register 
+const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
+const connectMongodbSession = require('connect-mongodb-session');
 
-// The app.use("/users", router) call means all valid URLs must start with the path /users
-app.use('/users', usersRouter); // middleware functions that have access to req & res
+// The app.use("/register", router) call means all valid URLs must start with the path /register
+app.use('/register', registerRouter); // middleware functions that have access to req & res
 app.use('/login', loginRouter);
 
 // -----------------Listening-----------------
--- routes/login.js
+++ routes/login.js
@@ -1,31 +1,42 @@
-const express = require('express');
+const express =require('express');
+const session = require('express-session');
 const { route } = require('express/lib/application');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const path = require('path');
 const date = new Date();
 
 // Importing a Schema
 const User = require('../models/user');
 
+//-------------Login-------------
 router.post('/', async (req, res)=>{
     try {
-        const user = await User.find({ 
-            username: req.body.username,
-            password: req.body.password
-        });
-        console.log("Login Try "+date.toLocaleTimeString());
-        console.log(user);
-        if (user.length != 0){
-            res.redirect('/loggedin');
-            console.log("Logged in successfully!!");
-        } else {
-            res.redirect('/404')
-            console.log("User Not Found");
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        console.log("\nLogin Try "+date.toLocaleTimeString());
+        
+        if(!user) {
+            console.log("User Not Found\n");
+            return res.redirect('/');
         };
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            console.log("Password is incorrect\n");
+            return res.redirect('/');
+        };
+
+        req.session.isAuth = true;
+        res.redirect('/loggedin');
+        
     } catch(err) {
         res.status(500).json({ message: err.message });
     };
 });
+
+
 
 
 
