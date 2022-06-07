9463bc1eb8a9a9108ef5a1e806c5df28f557c7c6
nws0078 <nws0078@gmail.com>
2022-06-07 12:47:40 -0400

    indentation update

--- routes/admin.js
+++ routes/admin.js
@@ -15,77 +15,77 @@ const User = require('../models/user');
 
 //----------------Login----------------
 router.post('/', async (req, res)=>{
-    console.log(req.body);
-    try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username });
-        console.log("Login Try "+date.toLocaleTimeString());
-        
-        if(!user) {
-            console.log("User Not Found\n");
-            return res.redirect('/404');
-        };
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if(!isMatch) {
-            console.log("Password is incorrect\n");
-            return res.redirect('/');
-        };
-
-        req.session.isAuth = true;
-        req.session.username = username;
-        console.log('Logged in successfully!')
-        res.redirect('/loggedin');
-        
-    } catch(err) {
-        res.status(500).json({ message: err.message });
+  console.log(req.body);
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    console.log("Login Try "+date.toLocaleTimeString());
+    
+    if(!user) {
+      console.log("User Not Found\n");
+      return res.redirect('/404');
     };
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+      console.log("Password is incorrect\n");
+      return res.redirect('/');
+    };
+
+    req.session.isAuth = true;
+    req.session.username = username;
+    console.log('Logged in successfully!')
+    res.redirect('/loggedin');
+      
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  };
 });
 
 //----------------Logout----------------
 router.post('/logout', (req, res) => {
-    req.session.destroy((err)=>{
-        if(err) throw err;
-        console.log('Logged out')
-        res.redirect('/');
-    });
+  req.session.destroy((err)=>{
+    if(err) throw err;
+    console.log('Logged out')
+    res.redirect('/');
+  });
 });
 
 
 //----------------Register----------------
 // Register (GET)
 router.get('/register', (req, res)=>{
-    res.sendFile(path.dirname(__dirname)+'/views/register.html');
+  res.sendFile(path.dirname(__dirname)+'/views/register.html');
 });
 
 // Register (POST)
 router.post('/register', async (req, res)=>{
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    userCheck = await User.findOne({username});
+  userCheck = await User.findOne({username});
 
-    if(userCheck) {
-        console.log("Your username is being used!")
-        return res.redirect('/register');
-    };
+  if(userCheck) {
+    console.log("Your username is being used!")
+    return res.redirect('/register');
+  };
 
-    const hashedPsw = await bcrypt.hash(password, 12); // 2nd param: salt
+  const hashedPsw = await bcrypt.hash(password, 12); // 2nd param: salt
 
-    const user = new User({
-        username,
-        password: hashedPsw
-    });
+  const user = new User({
+    username,
+    password: hashedPsw
+  });
     
-    try {
-        console.log("\nCreated User Account");
-        console.log("username: "+req.body.username);
-        console.log("password: "+req.body.password+"\n");
-        const newUser = await user.save();
-        res.redirect('/');
-    } catch(err){
-        res.status(400).json({ message: err.message })
-    };
+  try {
+    console.log("\nCreated User Account");
+    console.log("username: "+req.body.username);
+    console.log("password: "+req.body.password+"\n");
+    const newUser = await user.save();
+    res.redirect('/');
+  } catch(err){
+    res.status(400).json({ message: err.message })
+  };
 });
 
 
